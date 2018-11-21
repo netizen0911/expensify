445d7e924060117f80758e102c702e3c782a1b45
Paul G <paulo.guetan@gmail.com>
2018-11-21 23:18:34 +0800

    Added Redux exercises. Deconstructing and refactoring.

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,16 +1,40 @@
-const person = {
-  name: 'Andrew',
-  age: 27,
-  location: {
-    city: 'Philadelphia',
-    temp: 88
+// Object Destructuring
+
+// const person = {
+//   name: 'Andrew',
+//   age: 27,
+//   location: {
+//     city: 'Philadelphia',
+//     temp: 88
+//   }
+// };
+
+// const { name = 'Anonymous', age } = person;
+// console.log( `${name} is ${age}.` );
+
+// const { city, temp: temperature } = person.location;
+// if ( city && temperature ) {
+//   console.log(`It's ${temperature} in ${city}.`);
+// }
+
+const book = {
+  title: 'Ego is the Enemy',
+  author: 'Ryan Holiday',
+  publisher: {
+    name: 'Penguin'
   }
 };
 
-const { name = 'Anonymous', age } = person;
-console.log( `${name} is ${age}.` );
+const { name: publisherName = 'Self-Published' } = book.publisher;
+console.log(publisherName);
 
-const { city, temp: temperature } = person.location;
-if ( city && temperature ) {
-  console.log(`It's ${temperature} in ${city}.`);
-}
+
+// Array Destructuring
+
+const address = ['1299 S Jupiter Street', 'Philadelphia', 'Pennsylvania', '19147'];
+const [street, city, state = 'New York', zip] = address;
+console.log(`You are in ${city} ${state}.`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+const[drink, small, medium, large] = item;
+console.log(`A medium ${drink} costs ${medium}.`);
