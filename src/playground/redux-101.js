466da5447a06d305bc6e15dbdaa3e754017c7a12
Paul G <paulo.guetan@gmail.com>
2018-11-23 21:54:14 +0800

    Set up reducers.

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -19,7 +19,7 @@ const resetCount = () => ({
   type: 'RESET'
 }); 
 
-const store = createStore(( state = { count: 0 }, action) => {
+const countReducer = ( state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
@@ -39,9 +39,10 @@ const store = createStore(( state = { count: 0 }, action) => {
       };
     default:
       return state;
-  }  
-  
-});
+  }    
+};
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
   console.log(store.getState());
