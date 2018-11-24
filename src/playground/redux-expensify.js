74b18871d3c79fabba7e1fecc1a550a7ae8edfee
Paul G <paulo.guetan@gmail.com>
2018-11-24 22:03:57 +0800

    Set up object spread operator.

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -26,6 +26,19 @@ const removeExpense = ({ id } = {}) => ({
   id
 })
 
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: 'EDIT_EXPENSE',
+  id,
+  updates
+});
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+  type: 'SET_TEXT_FILTER',
+  text
+})
+
 // Expenses Reducer
 
 const expenseReducerDefaultState = [];
@@ -39,6 +52,17 @@ const expenseReducer = (state = expenseReducerDefaultState, action) => {
       ];
     case 'REMOVE_EXPENSE':
       return state.filter(({ id }) => id !== action.id);
+    case 'EDIT_EXPENSE':
+      return state.map((expense) => {
+        if (expense.id === action.id) {
+          return {
+            ...expense,
+            ...action.updates
+          }
+        } else {
+          return expense
+        }
+      });
     default:
       return state;
   }
@@ -55,6 +79,11 @@ const filterReducerDefaultState = {
 
 const filterReducer = (state = filterReducerDefaultState, action) => {
   switch (action.type) {
+    case 'SET_TEXT_FILTER':
+      return {
+        ...state,
+        text: action.text
+      };
     default:
       return state;
   }
@@ -78,6 +107,13 @@ const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 30
 
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount:500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
+
+
+
 
 // console.log(expenseOne);
 
