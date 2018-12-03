86f96d00df9836d46430943815a02e781eeccb93
Paul G <paulo.guetan@gmail.com>
2018-12-03 23:26:57 +0800

    rendering with react-redux

--- src/app.js
+++ src/app.js
@@ -11,9 +11,13 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water bill' }));
+store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
 store.dispatch(addExpense({ description: 'Gas Bill' }));
 store.dispatch(setTextFilter('water'));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter('bill'));
+}, 3000)
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
