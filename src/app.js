02a9c4963a13375a40d13435eaf9729199d4c1cb
Paul G <paulo.guetan@gmail.com>
2018-12-11 14:15:36 +0800

    Set up few more tests with Jest

--- src/app.js
+++ src/app.js
@@ -11,14 +11,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-// store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-// store.dispatch(addExpense({ description: 'Gas Bill', createAt: 1000 }));
-// store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
-
-const state = store.getState();
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
   <Provider store={store}>
     <AppRouter />
