b9ee46ecc50fa76ca3e74fee23c5e3eddb92b2f0
Paul G <paulo.guetan@gmail.com>
2018-12-19 14:48:17 +0800

    Setup testing environment with initial testing files.

--- src/tests/actions/filters.test.js
+++ src/tests/actions/filters.test.js
@@ -2,7 +2,9 @@ import moment from 'moment';
 import { 
   setStartDate, 
   setEndDate, 
-  setTextFilter 
+  setTextFilter,
+  sortByAmount,
+  sortByDate 
 } from '../../actions/filters';
 
 test('should generate set start date action object', () => {
@@ -28,4 +30,20 @@ test('should generate set text filter object with text value', () => {
     type: 'SET_TEXT_FILTER',
     text
   });
+});
+
+test('should generate set text filter object with default', () => {
+  const action = setTextFilter();
+  expect(action).toEqual({
+    type: 'SET_TEXT_FILTER',
+    text: ''
+  });
+});
+
+test('should generate action object for sort by date', () => {
+  expect(sortByDate()).toEqual({type: 'SORT_BY_DATE' })
+});
+
+test('should generate action object for sort by amount', () => {
+  expect(sortByAmount()).toEqual({type: 'SORT_BY_AMOUNT' })
 });
