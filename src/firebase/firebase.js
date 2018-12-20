fa57ff8fcc3b0e79d51ab3950e8579fbedbe2f3f
Paul G <paulo.guetan@gmail.com>
2018-12-20 22:11:04 +0800

    Set up test database.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,12 +2,12 @@ import * as firebase from 'firebase';
 import expenses from '../selectors/expenses';
 
 const config = {
-  apiKey: "AIzaSyAzlaMeuOAsorDQLfR_yXaZ3wWBRuYDNeU",
-  authDomain: "expensify-10ecc.firebaseapp.com",
-  databaseURL: "https://expensify-10ecc.firebaseio.com",
-  projectId: "expensify-10ecc",
-  storageBucket: "expensify-10ecc.appspot.com",
-  messagingSenderId: "929577673959"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
